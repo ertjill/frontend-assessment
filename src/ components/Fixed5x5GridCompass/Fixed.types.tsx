dd827b1d9a778c056fbac8033a712cd033701fc5
ertjill <110888298+emersonbenatiro@users.noreply.github.com>
2025-10-27 04:18:26 +0800

    correct comment
    correct comment

--- src/ components/Fixed5x5GridCompass/Fixed.types.tsx
+++ src/ components/Fixed5x5GridCompass/Fixed.types.tsx
@@ -1,7 +1,7 @@
 interface Grid5x5CompassProps {
-  x?: number; // number of columns
-  y?: number; // number of rows
-  direction?: 'NORTH' | 'EAST' | 'SOUTH' | 'WEST';
+  x?: number; // x axis
+  y?: number; // y axis
+  direction?: 'NORTH' | 'EAST' | 'SOUTH' | 'WEST'; //direction of the object
 }
 
 export type { Grid5x5CompassProps };
